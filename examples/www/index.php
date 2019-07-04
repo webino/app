0ded14258eafa19ab57950083862a9ecba981765
Sysadmin <sysadmin@wbpo.sk>
2019-07-04 17:58:40 +0200

    WIP

--- examples/www/index.php
+++ examples/www/index.php
@@ -19,6 +19,14 @@ require __DIR__ . '/../../vendor/autoload.php';
 chdir(__DIR__ . '/..');
 
 
+class ConsoleSpec implements InstanceFactoryMethodInterface
+{
+    public static function create(CreateInstanceEventInterface $event)
+    {
+        // TODO: Implement create() method.
+    }
+}
+
 abstract class AbstractConsoleCommand
 {
     public const NAME = '';
@@ -28,6 +36,67 @@ abstract class AbstractConsoleCommand
     public const CATEGORY = '';
 
     abstract public function onCommand(ConsoleEvent $event);
+}
+
+class DefaultCommand extends AbstractConsoleCommand
+{
+    /**
+     * @param ConsoleEvent $event
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
+    public function onCommand(ConsoleEvent $event)
+    {
+        $cli = $event->getConsole();
+        $cli->br()->boldUnderline('<yellow>Webino Console</yellow>')->br();
+
+        $arg = new ConsoleOption('version');
+        $arg->setPrefix('v');
+        $arg->setLongPrefix('version');
+        $arg->setDescription('Display version info.');
+        $arg->setNoValue();
+
+        $cli->arguments->add($arg->toArray());
+
+
+        $arg = new ConsoleOption('help');
+        $arg->setPrefix('h');
+        $arg->setLongPrefix('help');
+        $arg->setDescription('Display help.');
+        $arg->setNoValue();
+
+        $cli->arguments->add($arg->toArray());
+
+        $usageArgs = [];
+        /** @var \League\CLImate\Argument\Argument $arg */
+        foreach ($cli->arguments->all() as $arg) {
+            $usageArgs[] = '[-' . $arg->prefix() . '|--' . $arg->longPrefix() . ']';
+        }
+        $usageArgs = join(' ', $usageArgs);
+
+        $cli->out('<yellow>Usage:</yellow> php index.php ' . $usageArgs . ' <command> [<options>]')->br();
+
+        $cli->out('<yellow>Available commands:</yellow>');
+
+        $padding = $cli->padding(16)->char(' ');
+
+        /** @var ConsoleSpec $spec */
+        $spec = $app->get(ConsoleSpec::class);
+
+        foreach ($spec as $group => $subSpec) {
+
+            $cli->underline($group);
+
+            foreach ($subSpec as $label => $description) {
+                $cli->inline('   ');
+                $padding->label('<green>' . $label . '</green>')->result($description);
+            }
+        }
+
+        $cli->br();
+
+        return '';
+    }
 }
 
 class HelpCommand extends AbstractConsoleCommand
@@ -169,54 +238,13 @@ $app->onConsole(function (ConsoleEvent $event) {
     // TODO display help;
 //    return 'Help...';
 
-    $cli->br()->boldUnderline('<yellow>Webino Console</yellow>')->br();
-
-    $arg = new ConsoleOption('version');
-    $arg->setPrefix('v');
-    $arg->setLongPrefix('version');
-    $arg->setDescription('Display version info.');
-    $arg->setNoValue();
-
-    $cli->arguments->add($arg->toArray());
-
-
-    $arg = new ConsoleOption('help');
-    $arg->setPrefix('h');
-    $arg->setLongPrefix('help');
-    $arg->setDescription('Display help.');
-    $arg->setNoValue();
-
-    $cli->arguments->add($arg->toArray());
-
-    $usageArgs = [];
-    /** @var \League\CLImate\Argument\Argument $arg */
-    foreach ($cli->arguments->all() as $arg) {
-        $usageArgs[] = '[-' . $arg->prefix() . '|--' . $arg->longPrefix() . ']';
-    }
-    $usageArgs = join(' ', $usageArgs);
-
-    $cli->out('<yellow>Usage:</yellow> php index.php ' . $usageArgs . ' <command> [<options>]')->br();
-
-    $cli->out('<yellow>Available commands:</yellow>');
-
-    $padding = $cli->padding(16)->char(' ');
-
-    foreach ($spec as $group => $subSpec) {
-
-        $cli->underline($group);
-
-        foreach ($subSpec as $label => $description) {
-            $cli->inline('   ');
-            $padding->label('<green>' . $label . '</green>')->result($description);
-        }
-    }
-
-    $cli->br();
-
-
-
     // TODO invalid command error
-    return 'Invalid command';
+    //return 'Invalid command';
+
+    // default command
+    /** @var AbstractConsoleCommand $command */
+    $command = $app->make(DefaultCommand::class);
+    return $command->onCommand($event);
 
 
 
